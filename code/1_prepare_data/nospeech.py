b17a496bece69bb35e3558f3ed578d9e8d0eab34
moon <0917jong@gmail.com>
2024-07-24 02:17:39 +0900

    (refactor/moon) 주석 추가

--- code/1_prepare_data/nospeech.py
+++ code/1_prepare_data/nospeech.py
@@ -9,8 +9,7 @@ from tqdm import tqdm
 from torch.utils.data import DataLoader, Dataset
 from transformers import AutoFeatureExtractor, ASTForAudioClassification
 
-
-# Define dataset class
+# 데이터셋 클래스 정의
 class AudioDataset(Dataset):
     def __init__(self, file_paths, feature_extractor, target_sampling_rate=16000):
         self.file_paths = file_paths
@@ -26,11 +25,10 @@ class AudioDataset(Dataset):
         data = torchaudio.functional.resample(data, orig_freq=sr, new_freq=self.target_sampling_rate)
         data = data.squeeze()
         inputs = self.feature_extractor(data, sampling_rate=self.target_sampling_rate, return_tensors="pt")
-        inputs['input_values'] = inputs['input_values'].squeeze(0)  # Remove batch dimension
+        inputs['input_values'] = inputs['input_values'].squeeze(0)  # 배치 차원 제거
         return inputs['input_values'], file_path
 
-
-# Inference function
+# 추론 함수
 def inference_batch(model, dataloader):
     non_speeches = []
 
@@ -42,7 +40,7 @@ def inference_batch(model, dataloader):
         for i, logits in enumerate(outputs):
             logits = logits.squeeze()
 
-            #list_of_lists = [str(tensor.tolist()) for tensor in sorted(logits)[-k:]]
+            # logits을 정렬하여 인덱스를 얻음
             sorted_indices = torch.argsort(logits)
 
             # 0의 정렬된 인덱스에서의 위치를 찾음
@@ -58,27 +56,26 @@ def inference_batch(model, dataloader):
     
     return sorted(non_speeches)
 
-
 def main(args):
-    # Load model and feature extractor
+    # 모델 및 특징 추출기 로드
     feature_extractor = AutoFeatureExtractor.from_pretrained("MIT/ast-finetuned-audioset-10-10-0.4593")
     model = ASTForAudioClassification.from_pretrained("MIT/ast-finetuned-audioset-10-10-0.4593")
     model = model.to('cuda:0')
     model.eval()
 
-    # Parameters
+    # 파라미터 설정
     file_paths = glob(os.path.join(args.base_data_path, "test/*.ogg"))
     dataset = AudioDataset(file_paths, feature_extractor)
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False)
 
-    # Run inference
+    # 추론 실행
     non_speeches = inference_batch(model, dataloader)
 
-    # Create a DataFrame
+    # 데이터프레임 생성 및 CSV 저장
     pd.DataFrame(non_speeches, columns=['id']).to_csv(args.output_path, index=False)
 
-
 if __name__ == "__main__":
+    # 명령줄 인자 파싱
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch_size', type=int, default=32)
     parser.add_argument('--output_path', type=str, default="/root/asset/nospeech.csv")
